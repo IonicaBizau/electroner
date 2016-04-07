bb22537bb7ff968b12a4cc779d172b2d9797f161
Ionică Bizău <bizauionica@gmail.com>
2016-04-07 14:54:44 +0300

    Template strings

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 const electroner = require("../lib");
 
 // Start the Electron app
-electroner(__dirname + "/app/index.js", {
+electroner(`${__dirname}/app/index.js`, {
      "enable-transparent-visuals": true
    , "disable-cpu": true
 });
